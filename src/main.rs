9bea802d1e95ab62326d4a72f655604848397992
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-01-08 23:38:08 +0530

    :sparkles: generated a rust project + reading command line params

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+    let content = fs::read_to_string(file_path).expect("Should have returned some content");
+    println!("With text: {}", content);
+}
