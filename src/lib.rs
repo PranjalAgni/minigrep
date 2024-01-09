422f1f30a2c8348658bcdf2430cf505a9aa03ba7
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-01-09 23:10:09 +0530

    :green_heart: implemented search functionality + added tests

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,37 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    println!("Text: {}", content);
+    println!("File text: {}", content);
+    let results = search(&config.query, &content);
+    for result in results {
+        println!("\nMatch found ===> {result}");
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem passing argument {err}");
+        dbg!(err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
